5a8e918b91deb690343e07103dda1c35a14760a2
kukii77 <100643358+kukii77@users.noreply.github.com>
2022-04-03 19:04:50 +0100

    Update Main.java

--- week-9homwork/src/calculation/Main.java
+++ week-9homwork/src/calculation/Main.java
@@ -14,5 +14,23 @@ public class Main {
         char c= input.next().charAt(0);
         Calculator obj= new Calculator();
         obj.calculateResult(a,b,c);
+         System.out.println("Would you like to do more calculation Please enter “Y” or “N” :” ");
+
+        while (input.hasNext()) {
+            char d = input.next().charAt(0);
+            if (d == 'y') {
+                System.out.println("Enter first number :");
+                int e = input.nextInt();
+                System.out.println("Enter second number :");
+                int f = input.nextInt();
+                System.out.println("Please enter one of symbol +,-,*, /:");
+                char g = input.next().charAt(0);
+                Calculator obj1 = new Calculator();
+                obj.calculateResult(a, b, c);
+
+            } else {System.out.println("program is terminated ");
+            break;}
+            input.nextLine();
+        }
     }
 }
